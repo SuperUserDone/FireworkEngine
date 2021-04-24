a4daac3aa66f95a9d424d917d3936238530040f0
louis123456 <13525489+louis123456@users.noreply.github.com>
2021-04-24 18:07:27 +0200

    Increased buffer safety

--- engine/scene/component_mesh.hpp
+++ engine/scene/component_mesh.hpp
@@ -57,19 +57,28 @@ struct component_mesh
     {
         deletion_command cmd;
 
-        cmd.type = deletion_command::GPU_BUFFER;
-        cmd.id = render_data.index_buffer;
-        deletion_queue::get_queue().push_queue(cmd);
-        render_data.index_buffer = -1;
-
-        cmd.type = deletion_command::GPU_BUFFER;
-        cmd.id = render_data.vertex_buffer;
-        deletion_queue::get_queue().push_queue(cmd);
-        render_data.vertex_buffer = -1;
-
-        cmd.type = deletion_command::GPU_VAO;
-        cmd.id = render_data.vao;
-        deletion_queue::get_queue().push_queue(cmd);
-        render_data.vao = -1;
+        if (render_data.index_buffer != -1)
+        {
+            cmd.type = deletion_command::GPU_BUFFER;
+            cmd.id = render_data.index_buffer;
+            deletion_queue::get_queue().push_queue(cmd);
+            render_data.index_buffer = -1;
+        }
+        if (render_data.vertex_buffer != -1)
+        {
+            cmd.type = deletion_command::GPU_BUFFER;
+            cmd.id = render_data.vertex_buffer;
+            deletion_queue::get_queue().push_queue(cmd);
+            render_data.vertex_buffer = -1;
+        }
+        if (render_data.vao != -1)
+        {
+            cmd.type = deletion_command::GPU_VAO;
+            cmd.id = render_data.vao;
+            deletion_queue::get_queue().push_queue(cmd);
+            render_data.vao = -1;
+        }
     }
+
+    ~component_mesh() { queue_delete(); }
 };
