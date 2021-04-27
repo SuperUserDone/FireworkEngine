6ee12c920495a68191e9eb3fcadb31fbea0cb245
louis123456 <13525489+louis123456@users.noreply.github.com>
2021-04-27 17:03:49 +0200

    Added vfs

---
+++ engine/vfs/vfs.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "core/logger.hpp"
+#include <bits/types/FILE.h>
+#include <cstdio>
+#include <filesystem>
+#include <string>
+
+namespace blood
+{
+namespace vfs
+{
+
+static inline std::unordered_map<std::string, std::filesystem::path> file_lookup;
+
+inline void vfs_register(const std::string &path, const std::string &lookup)
+{
+    if (!std::filesystem::exists(path))
+        std::filesystem::create_directories(path);
+
+    file_lookup[lookup] = std::filesystem::path(path);
+}
+
+inline std::string vfs_resolve_path(const std::string &path)
+{
+    size_t a = path.find("://");
+
+    if (a == std::string::npos)
+    {
+        LOG_EF("Could not resolve path {}", path);
+        return "";
+    }
+
+    std::string lookup = path.substr(0, a);
+    std::string fspath = path.substr(a + 3);
+
+    auto location = file_lookup[lookup] / std::filesystem::path(fspath);
+
+    return location.string();
+}
+
+inline FILE *vfs_fopen(const std::string &path, const std::string &modes)
+{
+    return fopen(vfs_resolve_path(path).c_str(), modes.c_str());
+}
+
+} // namespace vfs
+} // namespace blood
