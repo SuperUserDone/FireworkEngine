30a6b317393ec0be8d370584df467b0149c5bf91
louis123456 <13525489+louis123456@users.noreply.github.com>
2021-05-16 16:20:48 +0200

    Make VFS parrallel

--- engine/vfs/vfs.hpp
+++ engine/vfs/vfs.hpp
@@ -3,21 +3,35 @@
 #include "core/logger.hpp"
 #include <cstdio>
 #include <filesystem>
+#include <mutex>
 #include <string>
+
+#include <parallel_hashmap/phmap.h>
 
 namespace blood
 {
 class vfs
 {
 public:
-    static inline std::unordered_map<std::string, std::filesystem::path> file_lookup;
+    static inline phmap::parallel_flat_hash_map<
+        std::string,
+        std::filesystem::path,
+        phmap::priv::hash_default_hash<std::string>,
+        phmap::priv::hash_default_eq<std::string>,
+        phmap::priv::Allocator<phmap::priv::Pair<const std::string, std::filesystem::path>>,
+        4,
+        std::mutex>
+        file_lookup;
 
     static inline void vfs_register(const std::string &path, const std::string &lookup)
     {
         if (!std::filesystem::exists(path))
             std::filesystem::create_directories(path);
 
-        file_lookup[lookup] = std::filesystem::path(path);
+        file_lookup.try_emplace_l(
+            lookup,
+            [path](std::filesystem::path &p) { p = std::filesystem::path(path); },
+            std::filesystem::path(path));
     }
 
     static inline std::string vfs_resolve_path(const std::string &path)
@@ -33,7 +47,17 @@ public:
         std::string lookup = path.substr(0, a);
         std::string fspath = path.substr(a + 3);
 
-        auto location = file_lookup[lookup] / std::filesystem::path(fspath);
+        std::filesystem::path p_lookedup;
+
+        if (!file_lookup.if_contains(lookup,
+                                     [&p_lookedup](std::filesystem::path &p) { p_lookedup = p; }))
+
+        {
+            LOG_EF("Could not resolve path {}", path);
+            return "";
+        }
+
+        auto location = p_lookedup / std::filesystem::path(fspath);
 
         return location.string();
     }
