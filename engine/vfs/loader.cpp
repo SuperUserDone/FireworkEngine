4ef38c28be6501350cd668222c1b14effdbf9faa
louis123456 <13525489+louis123456@users.noreply.github.com>
2021-07-14 11:06:59 +0200

    Memory order fix

--- engine/vfs/loader.cpp
+++ engine/vfs/loader.cpp
@@ -45,7 +45,7 @@ void loader::update()
 void loader::de_init()
 {
     LOG_I("Stopping loader");
-    m_running = false;
+    m_running.exchange(false, std::memory_order_acquire);
 
     int thread_n = 0;
 
@@ -68,7 +68,7 @@ void loader::worker(uint8_t id)
     LOG_DF("Starting thread {}", id);
 
     // TODO stats
-    while (m_running) {
+    while (m_running.load(std::memory_order_acquire)) {
         rate_limiter rate_limt(rate, &time);
 
         LOG_DF("Load rate thread {}: {}", id, time);
